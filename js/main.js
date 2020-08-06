430fef3630c7d8b452621cefb7b292e30e359eb1
cesarcisneros <cisnerosesteban351@gmail.com>
2020-08-05 19:57:39 -0500

    se realiza el index y paginas principales

---
+++ js/main.js
@@ -0,0 +1,28 @@
+'use stric'
+
+function Pagina(url) {
+    var pagina;
+
+
+    if (url == pagina1) {
+        pagina = window.open("pagina1.html", "ventana1", "width='400'", "height='300'");
+    } else if (url == pagina2) {
+        pagina = window.open("pagina2.html", "ventana1", "width='400'", "height='300'");
+    } else if (url == pagina3) {
+        pagina = window.open("pagina3.html", "ventana1", "width='400'", "height='300'");
+    } else if (url == pagina4) {
+        pagina = window.open("pagina4.html", "ventana1", "width='400'", "height='300'");
+    } else if (url == pagina5) {
+        pagina = window.open("pagina5.html", "ventana1", "width='400'", "height='300'");
+    } else if (url == pagina6) {
+        pagina = window.open("pagina6.html", "ventana1", "width='400'", "height='300'");
+    } else if (url == pagina7) {
+        pagina = window.open("pagina7.html", "ventana1", "width='400'", "height='300'");
+    } else if (url == pagina8) {
+        pagina = window.open("pagina8.html", "ventana1", "width='400'", "height='300'");
+    } else if (url == pagina9) {
+        pagina = window.open("pagina9.html", "ventana1", "width='400'", "height='300'");
+    } else if (url == pagina10) {
+        pagina = window.open("pagina10.html", "ventana1", "width='400'", "height='300'");
+    }
+}
